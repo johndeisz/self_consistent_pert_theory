7ae8c8b699c79e631ad8170252cdc22a133c92ea
John Deisz <john.deisz@gmail.com>
2022-07-05 12:14:19 +0200

    Minor update

--- input_files/src-tight-binding-gen/SrRu204_3-band/srru2o4-3band_ev.f
+++ input_files/src-tight-binding-gen/SrRu204_3-band/srru2o4-3band_ev.f
@@ -13,7 +13,6 @@ c Parameterization taken from Pavarini and Mazin, PRB 74 035115 (2006)
       t  = 0.0d0
       hop = 0.0d0
 
-      read(5,*) flux(1), flux(2), flux(3)
       read(5,*) ed(0), ed(1), ed(2)
       read(5,*) hop(:,1,0)
       read(5,*) hop(:,0,1)
@@ -48,9 +47,6 @@ c      hop = hop * 13.605698066d0 / 1000.0d0
         t(0,1,1,-1) = -t(0,1,1,-1)
         t(1,0,1,-1) = -t(1,0,1,-1)
 
-      write(6,*) '--------------flux values --------------'
-      write(6,*) flux(1), flux(2), flux(3)
-      write(6,*) 
       write(6,*) '--------------orbital level-------------'
       write(6,*) 0, ed(0)
       write(6,*) 1, ed(1)
@@ -58,8 +54,7 @@ c      hop = hop * 13.605698066d0 / 1000.0d0
 
       do ix = -2,2
         do iy = -2,2
-          write(6,*) 
-          write(6,200) ix,iy
+          write(6,200) ix,iy, 0
           do i1 = 0, 2
            do i2 = 0, 2 
           write(6,300) i1,i2,t(i1,i2,ix,iy),0.0d0
@@ -68,9 +63,8 @@ c      hop = hop * 13.605698066d0 / 1000.0d0
         enddo
       enddo
 
- 200  format('------------------- [',i3,',',i3,'] hopping',
-     $   '------------------------')
- 300  format(i3,',',i3,'  ','(',D16.9,',',D16.9,')')
+ 200  format(i3,'  ',i3,' ', i3)
+ 300  format(i3,'  ',i3,'   ',D16.9,D16.9)
 
 
       stop
